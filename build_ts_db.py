74cb4c576439d3b90a4cb2225d28b36c6e5a3ad1
Riccardo Setti <rsetti@gilt.com>
2013-02-25 23:23:29 +0000

    NOJIRA-0: server code generates valid js that can be displayed
    Added metric.html, basically is a snapshot of the highchart example but data(s)
    are generated on the fly by the server

--- build_ts_db.py
+++ build_ts_db.py
@@ -9,9 +9,9 @@ tn = telnetlib.Telnet("localhost", 4242)
 for ts in xrange(hourago, now, 5):
     rand = random.randint(1,5)
     val = random.randint(1000, 2000)
-    host = random.choice(["morte", "male", "distruzione"])
-    # put tsd.hbase.flushes 1361637805 0 host=Riccardo-Settis-MacBook-Pro.local
+    host = random.choice(["sun", "heart", "love"])
     theput = "put graphs.test.%d %d %d host=%s\n" % (rand, ts, val, host)
+    print(theput)
     tn.write(theput)
 
 tn.close()
--- server.go
+++ server.go
@@ -5,48 +5,85 @@ import (
 	"log"
 	"fmt"
 	"net/http"
+	"html/template"
 	"strings"
+	"strconv"
+	"time"
 )
 
+type Metric struct {
+	UnixTime int64
+	Year int
+	Month int
+	Day int
+	Hour int
+	Minute int
+	Second int
+	Value float64
+}
 
-func GetMetrics(query string) map[string]string {
-	toGraph := map[string]string {}
-	url := fmt.Sprintf("http://localhost:4242/q?%s", query)
+func buildMetric (m Metric, e []string) Metric {
+	m.Year, _ = strconv.Atoi(e[0])
+	m.Month, _ = strconv.Atoi(e[1]) // JS month starts from 0 fixme
+	m.Day, _ = strconv.Atoi(e[2])
+	m.Hour, _ = strconv.Atoi(e[3])
+	m.Minute, _ = strconv.Atoi(e[4])
+	m.Second, _ = strconv.Atoi(e[5])
+	return m
+}
+
+func parse_time(t string) string {
+	tm, err := strconv.Atoi(t)
+	if err != nil {
+		log.Println("Atoi failed: ", err)
+	}
+	ptime := time.Unix(int64(tm), 0).Format("2006,1,2,15,04,05")
+	return ptime
+}
+
+func getMetrics(query string) []Metric {
+	toGraph := []Metric{}
+
+	url := fmt.Sprintf("http://localhost:4242/q?%s", query) // should be a parameter
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Print("GET display: ", err)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
 	strBody := string(body)
 	metrics := strings.Split(strBody, "\n")
-	for m := range metrics {
-		if len(metrics[m]) > 2 {
-			metric := strings.Split(metrics[m], " ")
-			toGraph[metric[1]] = metric[2]
+
+	for i := range metrics {
+		if len(metrics[i]) > 2 {
+			m := Metric{}
+			metric := strings.Split(metrics[i], " ")
+			t := parse_time(metric[1])
+			m = buildMetric(m, strings.Split(t,","))
+			m.Value, _ = strconv.ParseFloat(metric[2], 64)
+			toGraph = append(toGraph, m)
 		}
 	}
-	defer resp.Body.Close()
 	return toGraph
 }
 
 func main(){
 
-	display := func(w http.ResponseWriter, req* http.Request){
+	displayHandler := func(w http.ResponseWriter, req* http.Request){
 		get := req.URL.Query()
 		req.Body.Close()
-		url := "?"
 
+		url := "?"
 		for k,v := range get {
 			url = fmt.Sprintf("%s&%s=%s", url, k, v[0] )
 		}
-		toGraph := GetMetrics(url)
-		for k,v := range toGraph {
-			fmt.Println(k,v)
-		}
+		toGraph := getMetrics(url)
+		t, _ := template.ParseFiles("metric.html")
+		t.Execute(w, toGraph)
 	}
 
-	http.HandleFunc("/display", display)
+	http.HandleFunc("/display", displayHandler)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
